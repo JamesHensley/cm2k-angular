8debcd4f9c50a3bea4a4c1ed94c816a8f9b085de
JamesHensley <JamesHensley@users.noreply.github.com>
2021-01-05 16:09:26 +0100

    Update test.js

--- src/resources/test.js
+++ src/resources/test.js
@@ -1,12 +1,13 @@
 function enumerateObject(obj, retObj) {
 	Object.keys(obj).forEach(o => {
-		let jStr = JSON.parse('{ "' + o.valueOf() + '": "' + (Array.isArray(obj[o]) ? 'Array' : typeof obj[o]) + '" }');
-		retObj.push(jStr);
+		let t = {};
+		t[o.valueOf()] = (Array.isArray(obj[o]) ? 'Array' : typeof obj[o]);
 
 		if(obj[o] && typeof obj[o] == 'object' && !Array.isArray(obj[o])) {
-			let enumeration = enumerateObject(obj[o], retObj);
-			[].concat(enumeration);
+			console.log(t);
+			t[o.valueOf()] = enumerateObject(obj[o], retObj);
 		}
+		retObj.push(t);
 	})
 	return retObj;
 }
